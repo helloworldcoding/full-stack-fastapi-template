c79d29fb6c239420be8ace3817d952c522f21b77
wm <mingazi@163.com>
2025-02-26 15:46:20 +0800

    abstract 5000 maxlenght

--- backend/app/models.py
+++ backend/app/models.py
@@ -126,7 +126,7 @@ class ResourceBase(SQLModel):
     # url: AnyHttpUrl = Field(unique=True, index=True, max_length=500)
     url: str = Field(unique=True, index=True, max_length=500)
     title: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default="", max_length=500)
+    description: str | None = Field(default="", max_length=5000)
     resource_type: str | None = Field(default="", max_length=50)  # rss url
     tags: list[str] | None = Field(default=[], sa_column=Column(ARRAY(String(50))))
     is_active: bool = True
@@ -160,7 +160,7 @@ class ArticleBase(SQLModel):
     resoure_id: str = Field(index=True, min_length=0, max_length=4024)
     url: str = Field(index=True, max_length=500)
     title: str = Field(min_length=1, max_length=255)
-    abstract: str | None = Field(default="", max_length=500)
+    abstract: str | None = Field(default="", max_length=5000)
     content: str | None = Field(default="")
     ai_abstract: str | None = Field(default="")
     ai_content: str | None = Field(default="")
